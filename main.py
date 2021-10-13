5067e014d9d90eeaff7f70fa3999d46f6d1063c5
Piero Morales <piero.morales@utec.edu.pe>
2021-10-13 13:17:45 -0500

    Avance filtros de Excel

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import pandas as pd
 
 if __name__ == "__main__":
-    data = pd.read_excel('REPORTE_COESTI.XLSX', sheet_name='SUGERIDO COESTI')
-    print(data)
+    # Lectura inicial archivo Excel de COESTI
+    excelInicial = pd.read_excel('Data_COESTI.xlsx', sheet_name='CL', header=None)
+    dataFrameInicial = pd.DataFrame(excelInicial)
+
+    # Se eliminan filas y columnas no necesarias
+    filtroFilas = dataFrameInicial.iloc[9:836, 1:37]
+    nombresColumnas = ['Centro', 'JOP', 'GOP', 'Transportes', 'Zona', 'Departamento', 'Distrito', 'Estación',
+                       'Material', 'Descripción', 'Producto', 'Concatenado', 'Capacidad DGH', 'Inventario Max Deseado',
+                       'Muerto', 'Varilla', 'Descarga', 'Stock Operativo', 'VTA Promedio Emergencia', 'X 1', 'X 2',
+                       'VTA Promedio %', 'VTA Día 04', 'VTA Día 05', 'Incremento VTA', 'Días Stock Emergencia',
+                       'VTA Normal', 'Días Stock Normal', 'Pedido', 'Sugerido', 'Días Stock 1', 'Días Stock 2', 'Y 1',
+                       'Cantidad Aproximada Carga', 'Observación', 'Z 1']
+
+    # Asignación de encabezados de todas las columnas
+    dataFrameEncabezado = pd.DataFrame(filtroFilas)
+    dataFrameEncabezado.columns = nombresColumnas
+
+    # Filtro de filas de zona Lima
+    dataFiltroZona = dataFrameEncabezado[dataFrameEncabezado['Zona'] == 'LIMA']
+
+    # Filtro de filas con cantidad sugerida diferente de nulo
+    dataFiltroSugerido = dataFiltroZona[dataFiltroZona['Sugerido'].notna()]
+
+    # Reiniciar los índices de las filas
+    indicesReiniciados = dataFiltroSugerido.reset_index(drop=True)
+    print(indicesReiniciados.to_string())
