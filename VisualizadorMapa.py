bd57d056cf8f5bb6940ac1659f0c6fdb3248e611
Piero Morales <piero.morales@utec.edu.pe>
2021-10-30 00:30:07 -0500

    Se agrega la visualización de rutas por unidad

--- VisualizadorMapa.py
+++ VisualizadorMapa.py
@@ -1,5 +1,6 @@
 import plotly.graph_objects as go
 import osmnx as ox
+import networkx as nx
 import json
 
 
@@ -9,8 +10,10 @@ class VisualizadorMapa:
         self.mapboxToken = credenciales['keyMapbox']
         self.dataFrame = dataFrame
         self.mapa = go.Figure()
+        self.grafo = ox.load_graphml('pruebas/grafoLima.graphml')
 
     def construirGrafo(self):
+        pass
         # maxLatitud = self.dataFrame['Latitud'].max()
         # minLatitud = self.dataFrame['Latitud'].min()
         #
@@ -27,8 +30,37 @@ class VisualizadorMapa:
 
         # G = ox.graph_from_bbox(norte, sur, este, oeste, network_type='drive')
         # ox.save_graphml(G, 'pruebas/grafo.graphml')
-        G = ox.load_graphml('pruebas/grafo.graphml')
+        # G = ox.load_graphml('pruebas/grafo.graphml')
         # ox.plot_graph(G)
+
+    def agregarCamino(self, origen, destino, unidad, color):
+        puntoOrigen = origen
+        puntoDestino = destino
+
+        nodoOrigen = ox.get_nearest_node(self.grafo, puntoOrigen)
+        nodoDestino = ox.get_nearest_node(self.grafo, puntoDestino)
+
+        ruta = nx.shortest_path(self.grafo, nodoOrigen, nodoDestino, weight='length')
+
+        latitudes = []
+        longitudes = []
+
+        for i in ruta:
+            punto = self.grafo.nodes[i]
+            latitudes.append(punto['y'])
+            longitudes.append(punto['x'])
+
+        self.mapa.add_trace(go.Scattermapbox(
+            name=unidad,
+            mode='lines',
+            lat=latitudes,
+            lon=longitudes,
+            marker={'size': 10},
+            line=dict(
+                width=3,
+                color=color
+            )
+        ))
 
     def visualizarEstaciones(self, columnaTexto):
         # Construir grafo para las rutas
@@ -37,20 +69,24 @@ class VisualizadorMapa:
         # Iniciar las posiciones de los puntos
         self.mapa = go.Figure(go.Scattermapbox(
             mode='markers',
+            showlegend=False,
             lat=self.dataFrame['Latitud'],
             lon=self.dataFrame['Longitud'],
-            text=self.dataFrame[columnaTexto]
+            text=self.dataFrame[columnaTexto],
+            marker={
+                'symbol': self.dataFrame['Símbolo'],
+                'size': 13,
+                'color': 'brown'
+            }
         ))
 
-        # Establecer los símbolos de las estaciones
-        self.mapa.update_traces(
-            marker_symbol=self.dataFrame['Símbolo'],
-            marker_size=13,
-            marker_color='brown',
-            selector=dict(
-                type='scattermapbox'
-            )
-        )
+        origenes = [(-12.05228029, -77.14360947), (-11.93553478, -77.12662366), (-12.13157737, -76.97689901)]
+        destinos = [(-12.116601, -77.04496341), (-11.97719639, -77.01030252), (-12.06775267, -76.94659696)]
+        unidades = ['AJF-705', 'B7K-982', 'AYR-771']
+        colores = ['blue', 'green', 'red']
+
+        for i in range(len(origenes)):
+            self.agregarCamino(origenes[i], destinos[i], unidades[i], colores[i])
 
         # Calcular centro del mapa
         promLatitud = self.dataFrame['Latitud'].mean()
@@ -59,6 +95,15 @@ class VisualizadorMapa:
         # Establecer configuraciones del mapa
         self.mapa.update_layout(
             margin={'l': 0, 't': 0, 'b': 0, 'r': 0},
+            legend=dict(
+                title=dict(
+                    font=dict(
+                        color='black',
+                        size=20
+                    ),
+                    text='Unidades'
+                )
+            ),
             mapbox=dict(
                 accesstoken=self.mapboxToken,
                 center=dict(
@@ -70,4 +115,4 @@ class VisualizadorMapa:
             ),
         )
 
-        # self.mapa.show()
+        self.mapa.show()
